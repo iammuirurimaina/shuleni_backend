9dfc79654be933e94fc29cb0fd476bda4b64cd34
hellenwamaitha <hellenwama254@gmail.com>
2023-10-25 15:30:18 +0300

    created model.py

---
+++ server/models.py
@@ -0,0 +1,8 @@
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.Integer)
