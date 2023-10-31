6a82c13818c586506f2de9c71b68e526345bb2b6
hellenwamaitha <hellenwama254@gmail.com>
2023-10-31 09:47:26 +0300

    edited the the student model to school model and added classes model

--- server/models.py
+++ server/models.py
@@ -6,41 +6,48 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
 
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(length=60), nullable=False)
+    email_address = db.Column(db.String, nullable=False, unique=True)
+    password_hash = db.Column(db.String, nullable=False)
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    role = db.relationship('Role', backref='user')
+    role = db.relationship('Role', backref='users')
 
 
-class Educator(db.Model):
+class School(db.Model, SerializerMixin):
 
-    __tablename__ = "instructors"
+    __tablename__ = "schools"
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    educator_number = db.Column(db.Integer(), nullable=False, unique=True) 
-    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(length=60), nullable=False)
-    phone_number = db.column (db.Integer(), nullable=False, unique=True)
+    school_name = db.Column(db.String, nullable=False)
+    poster = db.Column(db.String(), nullable=False, unique=True) 
+    location = db.Column(db.String, nullable=False, unique=True)
+    owner_id = db.Column(db.Interger, nullable=False)
+   
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    students = db.relationship('Student', backref='educator')
-
     
-    role = db.relationship('Role', backref='educator')
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+class CLasses(db.MOdel, SerializerMixin):
 
-    def __repr__(self):
-        return f'Educator(id={self.id}, name={self.name}, email_address={self.email_address})'
+    __tablename__ = "classes"
+
+    id = db.column(db.Integer(), primary_key=True)
+    class_name = db.Column(db.String, nullable=False)
+    educator_id = db.column(db.Integer, nullable=False)
+    school_id = db.colum(db.Integer, nullable=False)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+
