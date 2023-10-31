d169193390875dccc7d0c3bc096fe541bb53d88b
hellenwamaitha <hellenwama254@gmail.com>
2023-10-31 09:19:27 +0300

    created owner and Educator model

--- server/models.py
+++ server/models.py
@@ -1,1 +1,46 @@
-# models.py
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role = db.relationship('Role', backref='user')
+
+
+class Educator(db.Model):
+
+    __tablename__ = "instructors"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    educator_number = db.Column(db.Integer(), nullable=False, unique=True) 
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    phone_number = db.column (db.Integer(), nullable=False, unique=True)
+    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    students = db.relationship('Student', backref='educator')
+
+    
+    role = db.relationship('Role', backref='educator')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'Educator(id={self.id}, name={self.name}, email_address={self.email_address})'
