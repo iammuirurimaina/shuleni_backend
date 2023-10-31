1fa971b91b4b09e70a1cabd934a967aefa8b8a82
hellenwamaitha <hellenwama254@gmail.com>
2023-10-31 10:21:22 +0300

    created the resource and assessment model

--- server/models.py
+++ server/models.py
@@ -14,13 +14,13 @@ class User(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     email_address = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=False)
-    
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     role = db.relationship('Role', backref='users')
 
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    
 
 class School(db.Model, SerializerMixin):
 
@@ -37,7 +37,7 @@ class School(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     
-class CLasses(db.MOdel, SerializerMixin):
+class CLasses(db.Model, SerializerMixin):
 
     __tablename__ = "classes"
 
@@ -49,5 +49,41 @@ class CLasses(db.MOdel, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+class Assessments(db.Model, SerializerMixin):
 
+    __tablename__ = "Assessments"
+
+    id = db.column(db.Integer(), primary_key=True)
+    class_id = db.column(db.Integer(), nullable=False)
+    title = db.column(db.String, nullable=False)
+    body = db.column(db.string, nullable=False)
+    start_time = db.column(db.Integer, nullable=False)
+    end_time = db.column(db.Integer, nullable=False)
+    duration = db.column(db.Integer, nullable=False)
+
+    def __init__(self, class_id, title, body, start_time, end_time, duration):
+        self.class_id = class_id
+        self.title = title
+        self.body = body
+        self.start_time = start_time
+        self.end_time = end_time
+        self.duration = duration
+
+
+class Resource(db.Model, SerializerMixin):
+    
+    __tablename__ = 'resources'
+    resource_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    type = db.Column(db.String)
+    url = db.Column(db.String)
+    content = db.Column(db.String)
+    educator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    def __init__(self, title, type, url, content, educator_id):
+        self.title = title
+        self.type = type
+        self.url = url
+        self.content = content
+        self.educator_id = educator_id
 
