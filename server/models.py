e1a1a22472c24294ce8cb5dfe487bdc89f5810ae
hellenwamaitha <hellenwama254@gmail.com>
2023-10-31 11:14:14 +0300

    complete the models

--- server/models.py
+++ server/models.py
@@ -5,6 +5,11 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+class Role(db.Model, SerializerMixin):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column(db.String)
 
 class User(db.Model, SerializerMixin):
 
@@ -14,13 +19,11 @@ class User(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable=False)
     email_address = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
     role = db.relationship('Role', backref='users')
 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    
 
 class School(db.Model, SerializerMixin):
 
@@ -30,60 +33,103 @@ class School(db.Model, SerializerMixin):
     school_name = db.Column(db.String, nullable=False)
     poster = db.Column(db.String(), nullable=False, unique=True) 
     location = db.Column(db.String, nullable=False, unique=True)
-    owner_id = db.Column(db.Interger, nullable=False)
-   
-    
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    educators = db.relationship('User', backref= 'classes')
     
-class CLasses(db.Model, SerializerMixin):
+class Classes(db.Model, SerializerMixin):
 
     __tablename__ = "classes"
 
-    id = db.column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     class_name = db.Column(db.String, nullable=False)
-    educator_id = db.column(db.Integer, nullable=False)
-    school_id = db.colum(db.Integer, nullable=False)
-    
+    educator_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'), nullable= False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-class Assessments(db.Model, SerializerMixin):
+    educators = db.relationship('User', backref= 'classes')
+    schoools = db.relationship('School', backref = 'classes')
 
-    __tablename__ = "Assessments"
 
-    id = db.column(db.Integer(), primary_key=True)
-    class_id = db.column(db.Integer(), nullable=False)
-    title = db.column(db.String, nullable=False)
-    body = db.column(db.string, nullable=False)
-    start_time = db.column(db.Integer, nullable=False)
-    end_time = db.column(db.Integer, nullable=False)
-    duration = db.column(db.Integer, nullable=False)
+class Student_Class(db.Model, SerializerMixin):
+    
+    __tablename__ = 'student_classes'
 
-    def __init__(self, class_id, title, body, start_time, end_time, duration):
-        self.class_id = class_id
-        self.title = title
-        self.body = body
-        self.start_time = start_time
-        self.end_time = end_time
-        self.duration = duration
+    id = db.Column(db.Integer(), primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable= False)
+    student_id = db.Column(db.String, db.ForeignKey('users.id'), nullable= False)
 
+    classes = db.relationship('Class', backref='student_classes')
+    senders = db.relationship('User', backref= 'student_classes')
+
+
+class Attendance(db.Model, SerializerMixin):
+
+    __tablename__ = 'attendances'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable= False)
+    student_id = db.Column(db.String, db.ForeignKey('users.id'), nullable= False)
+    date = db.Column(db.DateTime)
+    is_present = db.Column(db.Boolean)
+
+    classes = db.relationship('Class', backref='attendances')
+    senders = db.relationship('User', backref= 'attendances')
 
 class Resource(db.Model, SerializerMixin):
-    
+
     __tablename__ = 'resources'
-    resource_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String)
     type = db.Column(db.String)
     url = db.Column(db.String)
     content = db.Column(db.String)
-    educator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    educator_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
 
-    def __init__(self, title, type, url, content, educator_id):
-        self.title = title
-        self.type = type
-        self.url = url
-        self.content = content
-        self.educator_id = educator_id
+    educators = db.relationship('User', backref= 'resources')
 
+class Assessment(db.Model, SerializerMixin):
+
+    __tablename__ = 'assessments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable= False)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
+    duration = db.Column(db.Integer)
+
+    classes = db.relationship('Class', backref='assessments')
+
+
+class Assessment_Response(db.Model, SerializerMixin):
+
+    __tablename__ = 'assessment_responses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    assessment_id = db.Column(db.Integer, db.ForeignKey('assessments.id'))
+    student_id = db.Column(db.String, db.ForeignKey('users.id'), nullable= False)
+    submitted_time = db.Column(db.DateTime)
+    work = db.Column(db.String)
+
+    asssessments = db.relationship('Assessment', backref='assesment_responses')
+    students = db.relationship('User', backref= 'assesment_responses')
+
+
+class Chat(db.Model, SerializerMixin):
+
+    __tablename__ = 'chats'
+
+    id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable= False)
+    sender = db.Column(db.String, db.ForeignKey('users.id'), nullable= False)
+    message = db.Column(db.String)
+
+    classes = db.relationship('Class', backref='chats')
+    senders = db.relationship('User', backref= 'chats')
+
+    
