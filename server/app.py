de74ff9c2291a800226c0b1c3e9c360699703018
hellenwamaitha <hellenwama254@gmail.com>
2023-10-31 12:28:43 +0300

    Added migrations

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
-# app.py
+from flask import Flask 
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
